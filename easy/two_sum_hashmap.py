073d0bea5ef7bcaa7d135c708b376dcb23cada3b
adil <adil21@ya.ru>
2024-03-28 12:20:59 +0300

    last

--- easy/two_sum_hashmap.py
+++ easy/two_sum_hashmap.py
@@ -8,23 +8,23 @@ class Solution(object):
         hashmap[nums[i]] = i
       else:
         valueindex = hashmap.pop(nums[i])
-        print(valueindex)
+        #print(valueindex)
         mylist = []
         mylist.append(valueindex)
         mylist.append(i)
         hashmap[nums[i]] = mylist
-        print(mylist)
+        #print(mylist)
     print(hashmap)
     for v in hashmap:
       if v < target:
         dop = target - v
         if dop in hashmap:
-          print('we are here 1')
+          #print('we are here 1')
           if type(hashmap[dop]) != list:
             if (hashmap[v] != hashmap[dop]):
               return [hashmap[v], hashmap[dop]]
           else:
-            print('we are here 2')
+            #print('we are here 2')
             if v == dop:
               mylist = hashmap[dop]
               print(mylist)
@@ -32,3 +32,5 @@ class Solution(object):
 
 s = Solution()
 print(s.twoSum([3,3,1], 6));
+
+print(s.twoSum([0,4,3,0], 0));
